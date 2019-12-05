3c805f13435303dbce1469246deb1ec4df9b26ca
Travis <travisstew214@gmail.com>
2019-12-04 22:45:40 -0500

    mobile media query

--- js/index.js
+++ js/index.js
@@ -1,24 +1,16 @@
 $(document).ready(function () {
- 
-
-   
-
-
-
-
-
 
   $(function() {
     $(window).scroll(function () {
        if ($(this).scrollTop() > 515) {
           $('.navbar a').css('color', 'black');
           $('.navbar').css('background','	rgb(245,245,245)');
- 
+          $('.burger-menu').css('background', 'black');
        }
        if ($(this).scrollTop() < 500) {
           $('.navbar').css('background','transparent');
           $('.navbar a').css('color', 'white');
-         
+          $('.burger-menu').css('background', 'white');
        }
     });
  });
