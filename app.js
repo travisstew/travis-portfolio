c34c32b0d3c67367abf6cd937520d32846728c8a
Travis <travisstew214@gmail.com>
2020-05-21 17:07:17 -0400

    update

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//body parser
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.listen(PORT,function () { 
+  console.log('listening on port ' + PORT);
+ });
